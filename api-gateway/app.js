4d4e86c7132abc6ffbc75d117db9fea67897c205
Vanitha Bai B C <vanitha.is22@bmsce.ac.in>
2025-05-25 00:48:07 +0530

    Update microservices

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -8,10 +8,10 @@ app.use(cors());
 app.use(express.json());
 
 // ✅ Deployed Service URLs on Render
-const locationServiceUrl = 'http://localhost:3001';
-const weatherServiceUrl = 'http://localhost:3002';
-const forecastServiceUrl = 'http://localhost:3003';
-const paymentServiceUrl = 'http://localhost:3004';
+const locationServiceUrl = 'https://smart-weather-v2-location.onrender.com';
+const weatherServiceUrl = 'https://smart-weather-v2-weather-services.onrender.com';
+const forecastServiceUrl = 'https://smart-weather-v2-forecast.onrender.com';
+const paymentServiceUrl = 'https://smart-weather-v2-payment-service.onrender.com';
 
 // ✅ Root Route
 app.get('/', (req, res) => {
